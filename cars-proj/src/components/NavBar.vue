d879a55f54fe361abbcdae659fe56b55666ae067
Your Name <yassinbez111@GMAIL.COM>
2023-04-14 06:36:05 +0100

    home-done

--- cars-proj/src/components/NavBar.vue
+++ cars-proj/src/components/NavBar.vue
@@ -14,7 +14,7 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">SERVICES</a>
+            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">OUR CARS</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">ABOUT US</a>
 
